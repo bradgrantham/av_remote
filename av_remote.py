55d6ada349f0871f4649750089dfc12a63ea4348
Brad Grantham <grantham@plunk.org>
2018-02-26 10:16:53 -0800

    Improve UI and behavior for threading and power on/off
    Add note about how to shutdown server cleanly via cURL
    
    Add threading to app run options to improve async behavior in backend
        Seems to improve async volume setting but I still don't understand
        why this is an issue
    
    Factor out disabling / enabling of all UI elements and call from
    every set function in frontend

--- av_remote.py
+++ av_remote.py
@@ -247,7 +247,7 @@ def shutdown():
 if __name__ == "__main__":
 
     if ("VISIBLEIP" in os.environ):
-        app.run(debug = True, port=5060, host="0.0.0.0")
+        app.run(debug = True, port=5060, host="0.0.0.0", threaded=True)
     else:
-        app.run(debug = True, port=5060)
+        app.run(debug = True, port=5060, threaded=True)
 
